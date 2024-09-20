37851b0b4d7c42b9fb71c574259bdfafa7961836
Dias203 <diasduong@gmail.com>
2024-09-20 23:51:47 +0700

    update lbl_img

--- hotel.py
+++ hotel.py
@@ -1,65 +1,70 @@
 from tkinter import *
-# from PIL import Image, ImageTk #pip install pillow
+from PIL import Image, ImageTk  # pip install pillow
 from customer import Cust_Win
 from room import Roombooking
 from detail import DetailsRoom
 
 class HotelManagementSystem:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
-        self.root.title("Hospital Management System")
+        self.root.title("Hotel Management System")
         self.root.geometry("1550x800+0+0")
 
-        #img1 = Image.open(C:\)
+        # Mở ảnh và thay đổi kích thước cho phù hợp với bề ngang
+        img1 = Image.open("G:\\Study\\Python Assignment\\BTL_Python\\images\\hotelimage.jpg")
+        img1 = img1.resize((1550, 140), Image.LANCZOS)  # Resize ảnh với bộ lọc LANCZOS
 
-        #Title
-        lbl_title = Label(self.root, text="HOTEL MANAGEMENT SYSTEM", font=("times new roman",40,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lbl_title.place(x=0,y=140,width=1550,height=50)
+        self.photoimg1 = ImageTk.PhotoImage(img1)
 
+        # Hiển thị ảnh trong Label
+        lbl_img = Label(self.root, image=self.photoimg1, bd=4, relief=RIDGE)
+        lbl_img.place(x=0, y=0, width=1550, height=140)
 
-        #Main Frame
-        main_frame=Frame(self.root,bd=4,relief=RIDGE)
-        main_frame.place(x=0,y=190,width=1550,height=620)
+        # Title
+        lbl_title = Label(self.root, text="HOTEL MANAGEMENT SYSTEM", font=("times new roman", 40, "bold"), bg="black", fg="gold", bd=4, relief=RIDGE)
+        lbl_title.place(x=0, y=140, width=1550, height=50)
 
-        #Menu
-        lbl_menu = Label(main_frame, text="MENU", font=("times new roman",20,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lbl_menu.place(x=0,y=0,width=230)
+        # Main Frame
+        main_frame = Frame(self.root, bd=4, relief=RIDGE)
+        main_frame.place(x=0, y=190, width=1550, height=620)
 
-        #btn_Frame
-        btn_frame=Frame(main_frame,bd=4,relief=RIDGE)
-        btn_frame.place(x=0,y=35,width=228,height=152)
+        # Menu
+        lbl_menu = Label(main_frame, text="MENU", font=("times new roman", 20, "bold"), bg="black", fg="gold", bd=4, relief=RIDGE)
+        lbl_menu.place(x=0, y=0, width=230)
 
-        #customer btn
-        cust_btn = Button(btn_frame,text="CUSTOMER",command=self.cust_details,width=22,font=("times new roman",14,"bold"),bg="black",fg="gold",bd=0,cursor="hand1")
-        cust_btn.grid(row=0,column=0,pady=1)
+        # btn_Frame
+        btn_frame = Frame(main_frame, bd=4, relief=RIDGE)
+        btn_frame.place(x=0, y=35, width=228, height=152)
 
-        room_btn = Button(btn_frame,text="ROOM",command=self.roombooking,width=22,font=("times new roman",14,"bold"),bg="black",fg="gold",bd=0,cursor="hand1")
-        room_btn.grid(row=1,column=0,pady=1)
+        # customer btn
+        cust_btn = Button(btn_frame, text="CUSTOMER", command=self.cust_details, width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        cust_btn.grid(row=0, column=0, pady=1)
 
-        details_btn = Button(btn_frame,text="DETAILS",command=self.detail_room,width=22,font=("times new roman",14,"bold"),bg="black",fg="gold",bd=0,cursor="hand1")
-        details_btn.grid(row=2,column=0,pady=1)
+        room_btn = Button(btn_frame, text="ROOM", command=self.roombooking, width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        room_btn.grid(row=1, column=0, pady=1)
 
-        logout_btn = Button(btn_frame,text="LOGOUT",command=self.logout,width=22,font=("times new roman",14,"bold"),bg="black",fg="gold",bd=0,cursor="hand1")
-        logout_btn.grid(row=3,column=0,pady=1)
+        details_btn = Button(btn_frame, text="DETAILS", command=self.detail_room, width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        details_btn.grid(row=2, column=0, pady=1)
 
+        logout_btn = Button(btn_frame, text="LOGOUT", command=self.logout, width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        logout_btn.grid(row=3, column=0, pady=1)
 
     def cust_details(self):
-        self.new_window= Toplevel(self.root)
-        self.app=Cust_Win(self.new_window)
+        self.new_window = Toplevel(self.root)
+        self.app = Cust_Win(self.new_window)
 
     def roombooking(self):
-        self.new_window= Toplevel(self.root)
-        self.app=Roombooking(self.new_window)
+        self.new_window = Toplevel(self.root)
+        self.app = Roombooking(self.new_window)
 
     def detail_room(self):
-        self.new_window= Toplevel(self.root)
-        self.app=DetailsRoom(self.new_window)
+        self.new_window = Toplevel(self.root)
+        self.app = DetailsRoom(self.new_window)
 
     def logout(self):
         self.root.destroy()
 
-
 if __name__ == "__main__":
-    root=Tk()
-    obj=HotelManagementSystem(root)
+    root = Tk()
+    obj = HotelManagementSystem(root)
     root.mainloop()
